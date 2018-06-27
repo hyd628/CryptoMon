099a419a014ef349a260049efd1bb27323d08efb
root <hyd628@gmail.com>
2018-06-27 02:12:57 +0000

    Start ignoring build folder

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "original garbage entry monster creek police delay talent maximum wink treat joy";
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,12 @@ module.exports = {
       port: 8545,
       gas: 2000000,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/m2J3nuxzfTtQZMdOBhMG")
+      },
+      network_id: 3
+    }   
   }
 };
