bd75f38ddee2842ceeb057b1ace51910b405e60d
hyd628 <hyd628@gmail.com>
2018-06-27 13:47:30 +0800

    layoutstyling

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -6,56 +6,64 @@
                     <div class="layout-logo"><img src="../assets/logo.png"></div>
                     <div class="layout-nav">
                         <menu-item name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
+                            <Icon type="ios-home-outline"></Icon>
+                            Home
                         </menu-item>
                         <menu-item name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
+                            <Icon type="ios-list-outline"></Icon>
+                            News
                         </menu-item>
                         <menu-item name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
+                            <Icon type="ios-information-outline"></Icon>
+                            Notifications
                         </menu-item>
                         <menu-item name="4">
-                            <Icon type="ios-paper"></Icon>
-                            Item 4
+                            <Icon type="ios-person-outline"></Icon>
+                            Account
                         </menu-item>
                     </div>
                 </Menu>
             </Header>
             <Layout>
-                <Sider hide-trigger :style="{background: '#495060'}">
+                <Sider width='200' :style="{background: '#495060'}">
                     <Menu theme="dark" width="auto">
                         <Submenu name="1">
                             <template slot="title">
-                                <Icon type="ios-navigate"></Icon>
-                                Item 1
+                                <Icon type="social-octocat"></Icon>
+                                Monsters
                             </template>
-                            <menu-item name="1-1">Option 1</menu-item>
-                            <menu-item name="1-2">Option 2</menu-item>
+                            <menu-item name="1-1">Spawn Monster</menu-item>
+                            <menu-item name="1-2">View Collection</menu-item>
                             <menu-item name="1-3">Option 3</menu-item>
                         </Submenu>
                         <Submenu name="2">
                             <template slot="title">
-                                <Icon type="ios-keypad"></Icon>
-                                Item 2
+                                <Icon type="ios-paw"></Icon>
+                                Battle Arena
                             </template>
                             <menu-item name="2-1">Option 1</menu-item>
                             <menu-item name="2-2">Option 2</menu-item>
                         </Submenu>
                         <Submenu name="3">
                             <template slot="title">
-                                <Icon type="ios-analytics"></Icon>
-                                Item 3
+                                <Icon type="cube"></Icon>
+                                Fortress
                             </template>
                             <menu-item name="3-1">Option 1</menu-item>
                             <menu-item name="3-2">Option 2</menu-item>
                         </Submenu>
+                        <Submenu name="4">
+                            <template slot="title">
+                                <Icon type="bag"></Icon>
+                                Auction House
+                            </template>
+                            <menu-item name="4-1">Option 1</menu-item>
+                            <menu-item name="4-2">Option 2</menu-item>
+                        </Submenu>
                     </Menu>
                 </Sider>
-                <Layout :style="{padding: '0 24px 24px'}">
-                    <Breadcrumb :style="{margin: '24px 0'}">
+                <Layout :style="{padding: '0 24px 0px'}">
+                    <Breadcrumb :style="{margin: '8px 0'}">
                         <BreadcrumbItem>Home</BreadcrumbItem>
                         <BreadcrumbItem>Components</BreadcrumbItem>
                         <BreadcrumbItem>Layout</BreadcrumbItem>
@@ -100,6 +108,7 @@
   text-align: center;
   max-height: 10px;
   font-size: 12px;
+  padding-top: 6px;
 }
 
 .layout-logo{
@@ -114,7 +123,9 @@
     bottom: 10px;
 }
 .layout-nav{
-    width: 420px;
+    /*width: 420px;*/
+    position: fixed;
+    right: 0px;
     margin: 0 auto;
     margin-right: 20px;
 }
